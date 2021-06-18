fbc213e53a30500f389a758f3f0e7ff60491fb92
KohZhen <kohzhen0102@gmail.com>
2021-06-18 15:49:26 +0800

    修改场景链接

--- TopDownShooter2D/Assets/Scripts/Exit2.cs
+++ TopDownShooter2D/Assets/Scripts/Exit2.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class Exit2 : MonoBehaviour
 {
     [SerializeField] private SceneFader sf;
-    private void OnTriggerEnter2D2(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
--- TopDownShooter2D/Assets/Scripts/Exit3.cs
+++ TopDownShooter2D/Assets/Scripts/Exit3.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class Exit3 : MonoBehaviour
 {
     [SerializeField] private SceneFader sf;
-    private void OnTriggerEnter2D2(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
